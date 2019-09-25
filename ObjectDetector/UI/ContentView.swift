065af671ad5f1aa9648bfe73a167a7d4ccf146a9
Pradnya <pradnyanikam@gmail.com>
2019-09-25 11:17:32 +0800

    Adds navigation bar and some UI cleanup

--- ObjectDetector/UI/ContentView.swift
+++ ObjectDetector/UI/ContentView.swift
@@ -15,17 +15,17 @@ struct ContentView: View {
   @State private var showImagePicker: Bool = false
   @State private var image: UIImage? = nil
   
-    var body: some View {
+  var body: some View {
+    NavigationView {
       VStack {
-        //View 1: Image
         if (image != nil) {
-          //View 2: Detection Result
-            if (imageProcessor.objectDetectionResult.isEmpty) {
-              Text("Processing...")
-            } else {
-              Text(imageProcessor.objectDetectionResult)
-            }
-          
+          //View 1: Detection Result
+          if (imageProcessor.objectDetectionResult.isEmpty) {
+            Text("Processing...")
+          } else {
+            Text(imageProcessor.objectDetectionResult)
+          }
+          //View 2: Image
           GeometryReader { geometry in
             Image(uiImage: self.image!)
               .resizable()
@@ -39,22 +39,24 @@ struct ContentView: View {
         }) {
           Text("Upload Image")
         }
-        .sheet(isPresented: $showImagePicker, onDismiss: {
-          //This is only called when the image picker is manually dismissed
-          self.showImagePicker = false
-        }, content: {
+          
           //View 4: Image Picker
-          ImagePicker(isShown: self.$showImagePicker, uiImage: self.$image, onDismiss: {
-            //Called when the image picker is programmatically dismissed
-            //for example after an image is chosen
+          .sheet(isPresented: $showImagePicker, onDismiss: {
+            //This is only called when the image picker is manually dismissed
+            self.showImagePicker = false
+          }, content: {
+            ImagePicker(isShown: self.$showImagePicker, uiImage: self.$image, onDismiss: {
+              //Called when the image picker is programmatically dismissed
+              //for example when an image is chosen
               self.showImagePicker = false
               if(self.image != nil) {
                 self.imageProcessor.processImage(image: self.image!)
               }
             })
           })
-      }
+      }.navigationBarTitle("Zoo App")
     }
+  }
 }
 
 #if DEBUG
